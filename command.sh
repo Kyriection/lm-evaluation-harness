5c1caf394fd0eb5cba1e10098bb9b554484ed65c
Allen <39230030+Kyriection@users.noreply.github.com>
2023-08-09 02:45:08 -0500

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,10 @@
 python main.py \
     --model hf-causal \
     --model_args pretrained=facebook/opt-350m \
-    --tasks hellaswag \
+    --tasks scrolls_contractnli \
     --device cuda:0
+
+
+
+
+# scrolls_contractnli scrolls_govreport scrolls_narrativeqa scrolls_qasper scrolls_qmsum scrolls_quality scrolls_summscreenfd
